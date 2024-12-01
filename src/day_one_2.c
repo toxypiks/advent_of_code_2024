e35e5653a62a56305ee18412a1f82117e7a04da6
toxypiks <HannaHinkelbein89@gmx.de>
2024-12-01 21:59:48 +0100

    Optimize code and compare run time

--- src/day_one_2.c
+++ src/day_one_2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 void read_input(int* values_first, int* values_second, size_t size)
 {
@@ -58,6 +59,43 @@ void read_input(int* values_first, int* values_second, size_t size)
   }
 }
 
+void swap(int *a, int *b) {
+  int tempvar = *a;
+  *a = *b;
+  *b = tempvar;
+}
+
+void heapify(int arr[], int n, int i) {
+  // Finding the greatest among root, left_side child, and right_side child of the tree
+  int greatest = i;
+  int left_side = 2 * i + 1;
+  int right_side = 2 * i + 2;
+
+  if (left_side < n && arr[left_side] > arr[greatest])
+    greatest = left_side;
+
+  if (right_side < n && arr[right_side] > arr[greatest])
+    greatest = right_side;
+
+  // Swap and continue heapifying if the root is not the greatest
+  if (greatest != i) {
+    swap(&arr[i], &arr[greatest]);
+    heapify(arr, n, greatest);
+  }
+}
+
+void heap_sort(int arr[], int n) {
+  // Build max heap
+  for (int i = n / 2 - 1; i >= 0; i--)
+    heapify(arr, n, i);
+
+  for (int i = n - 1; i >= 0; i--) {
+    swap(&arr[0], &arr[i]);
+
+    heapify(arr, i, 0);
+  }
+}
+
 void bubble_sort(int* values, size_t size)
 {
   for (size_t i=0; i < size; i++){
@@ -76,7 +114,7 @@ void bubble_sort(int* values, size_t size)
   }
 }
 
-int sum_occurency(int* value_first, int* values_second, size_t size)
+int sum_occurency_slow(int* value_first, int* values_second, size_t size)
 {
   int result = {0};
   for(size_t i = 0; i < size; ++i) {
@@ -92,17 +130,44 @@ int sum_occurency(int* value_first, int* values_second, size_t size)
   return result;
 }
 
+int sum_occurency_fast(int* value_first, int* values_second, size_t size)
+{
+  int result = {0};
+  int marker = {0};
+  for(size_t i = 0; i < size; ++i) {
+    int count = {0};
+    int number = value_first[i];
+    for(size_t j = marker; j < size; ++j) {
+      if (values_second[j] == number) {
+        count += 1;
+        marker = j;
+      }
+    }
+    result += number * count;
+  }
+  return result;
+}
 
 int main (void)
 {
+
   int values_first[1000] = {0};
   int values_second[1000] = {0};
   read_input(values_first, values_second, 1000);
-
+  clock_t time_stamp = clock();
+#if 0
   bubble_sort(values_first, 1000);
   bubble_sort(values_second, 1000);
+  int sum_occ = sum_occurency_slow(values_first, values_second, 1000);
+#else
+  heap_sort(values_first, 1000);
+  heap_sort(values_second, 1000);
+  int sum_occ = sum_occurency_fast(values_first, values_second, 1000);
+#endif
+  time_stamp = clock() - time_stamp;
+  double time_take = ((double)time_stamp)/CLOCKS_PER_SEC;
 
-  int sum_occ = sum_occurency(values_first, values_second, 1000);
-  printf("total sum occurency %d\n", sum_occ);
+  printf("total sum occurency %d time: %f\n", sum_occ, time_take);
+
   return 0;
 }
