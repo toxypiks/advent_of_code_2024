8a2ca1fd462c3445fe564801733b8df20a241144
toxypiks <HannaHinkelbein89@gmx.de>
2024-12-10 22:26:53 +0100

    Count XMAS and SAMX in all directions

--- src/day_four_1.c
+++ src/day_four_1.c
@@ -7,7 +7,7 @@ void read_matrix(char text[140][140])
   while(i < 140) {
     bool read_row = true;
     size_t j = {0};
-    while(read_row && j < 140) {
+    while(read_row && j < 141) {
       char buffer[2] = {0};
       fgets(buffer, 2, stdin);
       buffer[1] = 0; // 0 terminated
@@ -35,6 +35,40 @@ int check_for_xmas_samx(char window[4]) {
   return 0;
 }
 
+size_t get_row_length(size_t j, size_t col_length){
+  if(j < col_length/2){
+    return j+1;
+  } else{
+    return col_length - j;
+  }
+}
+
+char matrix_diagonal_right_at(size_t i,size_t j, char matrix[140][140], size_t length){
+  size_t x = 0;
+  size_t y = 0;
+  if( i < length/2){
+    x = i - j;
+    y = j;
+  } else {
+    x = length/2 - j;
+    y = (i - length/2) + j;
+  }
+  return matrix[y][x];
+}
+
+char matrix_diagonal_left_at(size_t i,size_t j, char matrix[140][140], size_t length){
+  size_t x = 0;
+  size_t y = 0;
+  if( i < length/2){
+    x = j;
+    y = (length/2 - i) + j;
+  } else {
+    x = (i - length/2) + j;
+    y = j;
+  }
+  return matrix[y][x];
+}
+
 int main(void)
 {
   int sum = 0;
@@ -43,7 +77,7 @@ int main(void)
 
   // check row wise
   for(size_t i = 0; i < 140; ++i){
-    for(size_t j = 0; j < 140 - 4; ++j) {
+    for(size_t j = 0; j < 140 - 3; ++j) {
       char window[4] = {0};
       for (size_t k = 0; k < 4; ++k) {
         window[k] = text[i][j + k];
@@ -51,19 +85,52 @@ int main(void)
       sum += check_for_xmas_samx(window);
     }
   }
-  printf("sum: %d\n", sum);
-  sum = 0;
+  printf("sum row: %d\n", sum);
 
   // check column wise
   for(size_t i = 0; i < 140; ++i){
-    for(size_t j = 0; j < 140 - 4; ++j) {
+    for(size_t j = 0; j < 140 - 3; ++j) {
       char window[4] = {0};
       for (size_t k = 0; k < 4; ++k) {
-        window[k] = text[j][i + k];
+        window[k] = text[j + k][i];
       }
       sum += check_for_xmas_samx(window);
     }
   }
+
+  printf("sum row + col: %d\n", sum);
+
+  int length = 2*140-1;
+  // check diagonal right
+  for(size_t i = 0; i < length; ++i){
+    size_t row_size = get_row_length(i,length);
+    if(row_size >=4 ){
+      for(size_t j = 0; j < row_size - 3; ++j) {
+        char window[4] = {0};
+        for (size_t k = 0; k < 4; ++k) {
+          window[k] = matrix_diagonal_right_at(i,j + k, text, length);
+        }
+        sum += check_for_xmas_samx(window);
+      }
+    }
+  }
+
+  printf("sum row + col + diag right: %d\n", sum);
+
+  // check diagonal left
+  for(size_t i = 0; i < length; ++i){
+    size_t row_size =  get_row_length(i,length);
+    if(row_size >=4 ){
+      for(size_t j = 0; j < row_size - 3; ++j) {
+        char window[4] = {0};
+        for (size_t k = 0; k < 4; ++k) {
+          window[k] = matrix_diagonal_left_at(i,j + k, text, length);
+        }
+        sum += check_for_xmas_samx(window);
+      }
+    }
+  }
+
   printf("sum: %d\n", sum);
   return 0;
 }
